06e54dc2963f8e2085dd1c27001fd00b9c010064
FlintShadey <125614527+FlintShadey@users.noreply.github.com>
2025-10-07 15:55:37 +0000

    Fix remaining asset paths in CSS fonts and service worker registration

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/MaidenVoyageAvailabilityCalendar/sw.js', { scope: '/MaidenVoyageAvailabilityCalendar/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
